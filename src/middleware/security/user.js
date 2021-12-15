0aa8d63cf4d9695155032b9cfedcab8c8337a78d
ImCookiieZ <53428372+ImCookiieZ@users.noreply.github.com>
2021-12-15 18:54:50 +0100

    fix service not online

--- src/middleware/security/user.js
+++ src/middleware/security/user.js
@@ -1,7 +1,6 @@
 import { db_adm_conn } from "../../modules/db/index.js";
 import { checkInputBeforeSqlQuery } from '../../modules/db/scripts.js';
 import Joi from "joi";
-import { checkInputBeforeSqlQuery } from "../../modules/db/scripts.js"
 const schema = Joi.object({
     firstName   : Joi.string()
                     .pattern(new RegExp('^[a-zA-Z\s\-]'))

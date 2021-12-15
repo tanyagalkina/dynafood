e88357a3c9ec53d9e5aeb150691c1d068ea47a53
tanyagalkina <tatjana.galkin@epitech.eu>
2021-12-15 13:33:59 +0100

    deleting src folder

--- db_start.sh
+++ db_start.sh
@@ -7,7 +7,15 @@ print_col() {
     tput sgr0
 }
 
-docker-compose down --remove-orphans && docker volume rm apidynafood_db_data
-print_col 2 "BUILDING DB ... => "
-docker-compose -f docker-compose.db.yaml up
+if ["$1" = "new"];
+    then
+        echo It was new
+        echo $1
+    else
+        echo It was NOT    
+fi
+
+#docker-compose down --remove-orphans && docker volume rm apidynafood_db_data
+#print_col 2 "BUILDING DB ... => "
+#docker-compose -f docker-compose.db.yaml up
 exit
